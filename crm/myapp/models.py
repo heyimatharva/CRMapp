fb41741599f73d1f0841ebf0ea56430cf10cfc59
heyimatharva <nigalatharva777@gmail.com>
2024-02-26 18:56:51 +0530

    create forms

--- crm/myapp/models.py
+++ crm/myapp/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=100)
